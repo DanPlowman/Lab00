5bcabeb43352056beffcfa3e1607571f8ea9636d
DanPlowman <47125582+DanPlowman@users.noreply.github.com>
2019-01-28 16:47:09 -0700

    Add files via upload

--- HelloGithub.py
+++ HelloGithub.py
@@ -1,12 +1,17 @@
-print("Hello Github!")
-file_squestns = open('StudentQuestions.txt', 'r')
-file_sanswers = open('StudentAnswers.txt', 'w')
-for line in file_squestns:
-  ans = input(line)
-  file_sanswers.write(ans + '\n') 
-print("Goodbye Github!")
-file_squestns.close()
-file_sanswers.close()
+#Daniel Plowman
+#COSC 2030
+#Jan 28, 2019
+#lab 00
+
+print("Hello Github!")								#outputs Hello Github! to the first line
+file_squestns = open('StudentQuestions.txt', 'r')	#defines file_squestns as a read file and opens 'StudentQuestions.txt' as that file
+file_sanswers = open('StudentAnswers.txt', 'w')		#same as above, but with writing and 'StudentAnswers.txt
+for line in file_squestns:							#Creates a for loop that repeats for each line in file_squestns
+  ans = input(line)									#Sets variable ans as the input received for the line of code
+  file_sanswers.write(ans + '\n') 					#Writes variable ans to the output file then goes down a line
+print("Goodbye Github!")							#Outputs Goodbye Github! to the last line
+file_squestns.close()								#saves and closes the input file
+file_sanswers.close()								#saves and closes the output file
 
 
 
